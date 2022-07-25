1c6ad99e078269ae138a28476b7da2acf4ea4b22
rujipas <rujipas1534@gmail.com>
2022-07-26 00:23:30 +0700

    1

--- src/main/java/project/isns/SmtpMailSender.java
+++ src/main/java/project/isns/SmtpMailSender.java
@@ -1,6 +1,7 @@
 package project.isns;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -14,12 +15,19 @@ public class SmtpMailSender {
     @Autowired
     private JavaMailSender javaMailSender;
     public  void send(String to,String subject, String body) throws MessagingException {
-        MimeMessage message = javaMailSender.createMimeMessage();
-        MimeMessageHelper helper;
-        helper = new MimeMessageHelper(message,true);
-        helper.setSubject(subject);
-        helper.setTo(to);
-        helper.setText(body,true);
+//        MimeMessage message = javaMailSender.createMimeMessage();
+//        MimeMessageHelper helper;
+//        helper = new MimeMessageHelper(message,true);
+//        message.setSubject(subject);
+//        message.setTo(to);
+//        helper.setText(body,true);
+//        javaMailSender.send(message);
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("rujipas1534@gmail.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
         javaMailSender.send(message);
     }
 }
